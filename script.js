fc0b403e5d37e30b9e11b5d2fa747947bf8e971e
Diamons-Gold <75449047+Diamons-Gold@users.noreply.github.com>
2021-03-13 16:12:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("white-mode");
+function displayFunction() {
+	var x = document.getElementById("theme");
+	if (x.style.opacity === "1") {
+		x.style.opacity = "0";
+		x.style.bottom = "70%";
+	} else {
+		x.style.opacity = "1";
+		x.style.bottom = "60%";
+	}
+
 }
+
+function themeFunction() {
+	var element = document.getElementsByTagName("html")[0];
+	element.classList.toggle("white-mode");
+}
+
+function playAudio() { 
+	var y = document.getElementById("myAudio"); 
+	y.play(); 
+}
+
